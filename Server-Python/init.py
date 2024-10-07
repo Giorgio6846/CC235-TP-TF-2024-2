34a65671d04444242ea6bc7a009cf3b311a7ac8c
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-10-06 23:31:32 -0500

    Added Server

--- Server-Python/init.py
+++ Server-Python/init.py
@@ -2,28 +2,26 @@ from flask import Flask, request, jsonify
 import json
 from PIL import Image
 import io
+import os
 
 app = Flask(__name__)
 
-@app.route("/face-depth-data", methods = ['POST'])
+@app.route("/face-depth-data", methods=["POST"])
 def face_data():
-    if 'depthData' not in request.form or 'image' not in request.files:
+    if "faceImage" not in request.files or "depthImage" not in request.files:
         return jsonify({"error": "Falta datos"}), 400
-    
-    depth_data = json.load(request.form["depthData"])
-    
-    print(request.form)
-    
-    with open("./Data/FaceDepth.json", "w") as outfile:
-        json.dump(depth_data, outfile)
 
-    print("Datos de profundidad", depth_data)
+    depth_file = request.files["depthImage"]
+    depth_image = Image.open(io.BytesIO(depth_file.read()))
+    depth_image = depth_image.rotate(270)
+    depth_image.save(os.path.join(os.path.dirname(__file__), "data", "DepthImage.jpg"))
 
-    image_file = request.files["image"]
-    image = Image.open(io.BytesIO(image_file.read()))
-    image.save("./Data/FaceImage.jpg")
+    colorImage_file = request.files["faceImage"]
+    colorImage = Image.open(io.BytesIO(colorImage_file.read()))
+    colorImage = colorImage.rotate(270)
+    colorImage.save(os.path.join(os.path.dirname(__file__), "data", "FaceImage.jpg"))
 
     return jsonify({"Message": "Datos recibidos con exito"})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050)
